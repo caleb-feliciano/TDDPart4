cc712e04c5f0331a13ede4b2792279cfd013cf25
jpate128 <77931182+jpate128@users.noreply.github.com>
2021-03-08 20:44:17 -0500

    Add files via upload

--- Invoice.py
+++ Invoice.py
@@ -1,44 +1,40 @@
 class Invoice:
+
     def __init__(self):
         self.items = {}
-        
+
     def addProduct(self, qnt, price, discount):
         self.items['qnt'] = qnt
         self.items['unit_price'] = price
         self.items['discount'] = discount
         return self.items
-    
+
     def totalImpurePrice(self, products):
         total_impure_price = 0
         for k, v in products.items():
             total_impure_price += float(v['unit_price']) * int(v['qnt'])
         total_impure_price = round(total_impure_price, 2)
         return total_impure_price
-        
+
     def totalDiscount(self, products):
         total_discount = 0
-        
-        for k,v in products.items():
+        for k, v in products.items():
             total_discount += (int(v['qnt']) * float(v['unit_price'])) * float(v['discount']) / 100
         total_discount = round(total_discount, 2)
         self.total_discount = total_discount
         return total_discount
-        
+
     def totalPurePrice(self, products):
-        total_pure_price = self.totalImpurePrice(products) - self.totalDiscount(products)
+        total_pure_price = self.totalImpurePrice(products)-self.totalDiscount(products)
         return total_pure_price
-        
-    def percentDiscount(self, products):
-        percent_discount = self.totalDiscount(products) * 100 / self.totalPurePrice(products)
-        return percent_discount
-    
+
     def inputAnswer(self, input_value):
         while True:
             userInput = input(input_value)
             if userInput in ['y', 'n']:
                 return userInput
             print("y or n! Try again.")
-            
+
     def inputNumber(self, input_value):
         while True:
             try:
@@ -48,4 +44,21 @@ class Invoice:
                 continue
             else:
                 return userInput
-         
+    # JayPatel- This adds the total amount of tax together
+    def addTotalTax (self, products):
+        sales_tax = float(0.0475 * self.totalPurePrice(products))
+        total_sales_tax = round(sales_tax, 2)
+        county_tax = float(0.02 * self.totalPurePrice(products))
+        total_county_tax = round(county_tax, 2)
+        total_tax = total_sales_tax + total_county_tax
+        self.total_tax = round(total_tax, 2)
+        return self.total_tax
+
+
+    # Jay Patel- This adds the total amount of price with tax together
+    def calculateTotalTax(self, products):
+        total_sale = self.totalPurePrice(products) + self.addTotalTax(products)
+        return total_sale
+
+
+
--- TestInvoice.py
+++ TestInvoice.py
@@ -1,33 +1,41 @@
 import pytest
 from Invoice import Invoice
 
-
-@pytest.fixture()
-def invoice():
-    invoice = Invoice()
-    return invoice
-    
 @pytest.fixture()
 def products():
     products = {'Pen': {'qnt': 10, 'unit_price': 3.75, 'discount': 5},
                 'Notebook': {'qnt': 5, 'unit_price': 7.5, 'discount': 10}}
     return products
-    
 
-def test_CanCalcucalateTotalImpurePrice(invoice, products):
+# def test_CanFindInvoiceClass():
+    # invoice = Invoice()
+
+@pytest.fixture()
+def invoice():
+    invoice = Invoice()
+    return invoice
+
+def test_CanCalculateTotalImpurePrice(invoice, products):
+    # invoice = Invoice()
     invoice.totalImpurePrice(products)
     assert invoice.totalImpurePrice(products) == 75
 
-def test_CanCalcucateTotalDiscount(invoice, products):
+def test_CanCalculateTotalDiscount(invoice, products):
     invoice.totalDiscount(products)
     assert invoice.totalDiscount(products) == 5.62
 
-def test_CanCalcucalateTotalPurePrice(invoice, products):
+def test_CanCalculateTotalPurePrice(invoice, products):
     invoice.totalPurePrice(products)
     assert invoice.totalPurePrice(products) == 69.38
 
-def test_CanCalcucalatePercentDiscount(invoice, products):
-    invoice.percentDiscount(products)
-    assert invoice.percentDiscount(products) == 8.100317094263477
+# Jaypatel- This is test for making sure that tax amount adds up correctly.
+def test_addTotalTax(invoice, products):
+    invoice.addTotalTax(products)
+    assert invoice.addTotalTax(products) == 4.69
+
+# Jaypatel- This is test for making sure that the total amount with tax is displayed correcly.
+def test_canCalculateTax (invoice, products):
+    invoice.calculateTotalTax(products)
+    assert invoice.calculateTotalTax(products) == 74.07
 
 
